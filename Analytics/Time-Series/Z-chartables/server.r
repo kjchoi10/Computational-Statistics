2e68b5b96efb6bc5dc117b9c9b3d27e8db568564
Kevin <kjchoi10@gmail.com>
2015-04-19 09:52:22 -0400

    Zchartables Analytics

--- Analytics/Time-Series/Z-chartables/server.r
+++ Analytics/Time-Series/Z-chartables/server.r
@@ -1,23 +1,100 @@
-library(shiny)
+#Forecast algorithm
+z.charting <- function(salesAnalysisPrevious, salesAnalysisRecent){
+  # Previous Year Total
+  previous.year.total <- sum(salesAnalysisPrevious)
+  
+  # Cumulative
+  cumulative <- cumsum(salesAnalysisRecent)
+  
+  # Moving Total Average
+  moving.average <- rep(0, length(salesAnalysisRecent))
+  
+  # Computes the first indexed moving average
+  moving.average[1] <- previous.year.total + salesAnalysisRecent[1] - salesAnalysisPrevious[1]
+  
+  # Initiate at 2
+  i <- 2
+  
+  # Computes the moving average for index 2 using the pre-calculated moving average at index 1
+  repeat {
+    
+    moving.average[i] <- moving.average[i - 1] + salesAnalysisRecent[i] - salesAnalysisPrevious[i]
+    
+    if(i == length(salesAnalysisPrevious)) break
+    
+    i <- i + 1
+    
+  }
+  
+  return(moving.average) 
+}
+
+## server.r
 
 shinyServer(function(input, output) {
   
-  output$table.output <- renderTable({
-    
-    inFile <- input$file1
-    
-    if (is.null(inFile))
-    return(NULL)
-    
-    read.csv(inFile$datapath, header=input$header, sep=input$sep, quote=input$quote)
-  })
-
-  output$print <- renderTable({
-    previous.year <- input$inFile[1]# stuck on how to get the fourth column of data in the csv
-    #current.year <- 
-    print(previous.year)
+  #This function is repsonsible for loading in the selected file
+  filedata <- reactive({    
+    infile <- input$datafile
+    if (is.null(infile)) {
+      # User has not uploaded a file yet
+      return(default.frame)
+    }
+    read.csv(infile$datapath)
   })
   
-
+  #This previews the CSV data file
+  output$filetable <- renderTable({
+    filedata()
+  })
   
+  #This allows users to select the recent sales column
+  output$selectCol1 <- renderUI({
+    df <- filedata()
+    if (is.null(df)) return(NULL)
+    
+    items=names(df)
+    selectInput("selectRecent", "Select Recent Sales:",items)
+    
+  })
+  
+  #This allows users to select the previous sales column
+  output$selectCol2 <- renderUI({
+    df <- filedata()
+    if (is.null(df)) return(NULL)
+    
+    items=names(df)
+    selectInput("selectPrevious", "Select Previous Sales:",items)
+  })
+  
+  
+##
+# This code is executed whenever the salesAnalysis Tab is opened. 
+# It will collect the data.frame from filedata(), read the chosen
+# column names from the input widgets, and pass the chosen columns
+# to the z.charting function.
+# 
+# Plot the results of z.charting()
+#
+
+
+  #This allows the analysis to begin
+  output$salesAnalysis <- renderPlot({
+    prev <- input$selectPrevious
+    recent <- input$selectRecent
+    print(names(input))
+    print(prev)
+    print(recent)
+    df <- filedata()
+    print(df[c(prev, recent)])
+    
+    # Any logic for preparing the data for use in z.charting goes here.
+    
+    z.chart.data <- z.charting(df[,prev], df[,recent])
+    plot(z.chart.data, xlab= "Months", xlim = range(1:12), 
+         ylab = "Moving Average Sales", ylim = range(0:max(z.chart.data)), main = "Moving Average Trend", col= "blue")
+    
+    
+  })
 })
+  
--- Analytics/Time-Series/Z-chartables/ui.r
+++ Analytics/Time-Series/Z-chartables/ui.r
@@ -1,19 +1,22 @@
-library(shiny)
-
 shinyUI(pageWithSidebar(
-  headerPanel('Upload Files'),
-      sidebarPanel(
-        fileInput('file1', 'Choose CSV File', 
-        accept=c('text/csv', 'text/comma-separated-values', 'ues', 'text/plain', '.csv')),
-        checkboxInput('header', 'Header', TRUE),
-        radioButtons('sep', 'Seperator', c(Comma=',', Semicolon=';', Tab='\t'), ','),
-        radioButtons('quote', 'Quote', c(None='','Double Quote'='"','Single Quote'="'"),'"')),
-        
-        mainPanel(
-        tableOutput('table.output')
-        )
-      ))
-
-
-
-
+  headerPanel("Zchartables"),
+  
+  sidebarPanel(
+    #Selector for file upload
+    fileInput('datafile', 'Choose CSV file',
+              accept=c('text/csv', 'text/comma-separated-values,text/plain')),
+    #These column selectors are dynamically created when the file is loaded
+    uiOutput("selectCol1"),
+    uiOutput("selectCol2"),
+    #The action button allows you to start analyzing
+    actionButton("analysis", "Begin Forecast")
+    
+  ),
+  
+  mainPanel(
+    tabsetPanel(
+      tabPanel("File Table",tableOutput("filetable")), 
+      tabPanel("Analysis", plotOutput("salesAnalysis"))
+    )
+  )
+))
